cfe6d144edaad40398d9bad610ef920e9dc3c0a0
Merhawi Fissehaye <mfissehaye@apposit.com>
2015-10-13 10:49:26 +0300

    angular routing, timeline

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by mfissehaye on 10/13/15.
+ */
+app = angular.module('malam', ['ngRoute', 'angular-spinkit'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.tmp.html'
+            })
+            .when('/who', {
+                templateUrl: 'views/who.tmp.html'
+            })
+            .when('/what', {
+                templateUrl: 'views/what.tmp.html'
+            })
+            .when('/where', {
+                templateUrl: 'views/where.tmp.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
