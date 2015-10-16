0bc8ad6e78fb4adf9129ad5dff723d29ac5b8bb7
Merhawi Fissehaye <mfissehaye@apposit.com>
2015-10-16 10:04:30 +0300

    Added a blog like page

--- js/app.js
+++ js/app.js
@@ -10,9 +10,11 @@ app = angular.module('malam', ['ngRoute', 'angular-spinkit'])
             .when('/who', {
                 templateUrl: 'views/who.tmp.html'
             })
-            .when('/what', {
-                templateUrl: 'views/what.tmp.html',
-                controller: 'WhatController'
+            .when('/what/:serviceName', {
+                templateUrl: function(params) {
+                    var serviceName = params.serviceName ? params.serviceName : 'what';
+                    return 'views/' + serviceName + '.tmp.html'
+                }
             })
             .when('/where', {
                 templateUrl: 'views/where.tmp.html'
@@ -20,4 +22,67 @@ app = angular.module('malam', ['ngRoute', 'angular-spinkit'])
             .otherwise({
                 redirectTo: '/'
             });
+    }]).
+    controller('WhatController', ['$scope', function($scope) {
+        var whatUrlPrefix = '#/what/';
+        $scope.whatProducts = [
+            {
+                url: whatUrlPrefix + 'erp/',
+                placeholder: 'Enterprise Resource Planning (ERP)',
+                img: ''
+            },
+            {
+                url: whatUrlPrefix + 'fleet/',
+                placeholder: 'Fleet Management System (Intelligent Transportation System)',
+                img: ''
+            },
+            {
+                url: whatUrlPrefix + 'ntk/',
+                placeholder: 'Networking Equipment',
+                img: ''
+            },
+            {
+                url: whatUrlPrefix + 'cable/',
+                placeholder: 'Cable Pathways',
+                img: ''
+            },
+            {
+                url: whatUrlPrefix + 'power/',
+                placeholder: 'Electrical Power Distribution Systems',
+                img: ''
+            },
+            {
+                url: whatUrlPrefix + 'dc/',
+                placeholder: 'Data Center Facility',
+                img: ''
+            },
+            {
+                url: whatUrlPrefix + 'security/',
+                placeholder: 'Safety and Security Solutions',
+                img: ''
+            },
+            {
+                url: whatUrlPrefix + 'unicom/',
+                placeholder: 'Unified Communication Solutions',
+                img: ''
+            }
+        ];
+
+        $scope.whatServices = [
+            {
+                url: whatUrlPrefix + 'web/',
+                placeholder: 'Web Application and Website Development',
+                img: ''
+            },
+            {
+                url: whatUrlPrefix + 'host/',
+                placeholder: 'Web Hosting',
+                img: ''
+            },
+            {
+                url: whatUrlPrefix + 'consult/',
+                placeholder: 'Consultancy & Project Management',
+                img: ''
+            }
+        ];
     }]);
